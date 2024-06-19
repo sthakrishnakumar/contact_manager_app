3cf65cc2621d9da8a6df6696076feaf3ff5f638f
Krishna <krishna@Krishnas-MacBook-Air.local>
2024-06-19 20:07:26 +0545

    chore : react project created and basic component created

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import Contact from "../components/Contact";
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <h3>Contact List :</h3>
+        <Contact />
+        <Contact />
+        <Contact />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="contact1">
+        Ram
+        <br />
+        9843093331
+        <br />
+        Balaju
+        <br />
+        <br />
+      </div>
+    </>
+  );
+};
+
+export default Contact;
